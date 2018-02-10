0ec7401dfbcff2b268c4df73b2e9be1fab4fd16a
Alexsander Akers <me@a2.io>
2018-02-10 19:54:30 +0100

    Commit doggos

---
+++ Random Dog/ImageViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageViewCell.swift
+//  Random Dog
+//
+//  Created by Alexsander Akers on 2/10/18.
+//  Copyright © 2018 Alexsander Akers. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
--- Random Dog/ViewController.swift
+++ Random Dog/ViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    @IBOutlet var collectionView: UICollectionView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: - Collection View
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 50
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DogCell", for: indexPath) as! ImageViewCell
+        cell.imageView.image = UIImage(named: "dog\(indexPath.item + 1)")
+        return cell
     }
-
-
 }
-
