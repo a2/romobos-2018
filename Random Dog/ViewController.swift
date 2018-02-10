ff5aeafd8bd31c4125bce7fa2dfd6712ee0fdfbe
Alexsander Akers <me@a2.io>
2018-02-10 18:48:00 +0100

    Initial commit

---
+++ Random Dog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Random Dog
+//
+//  Created by Alexsander Akers on 2/10/18.
+//  Copyright © 2018 Alexsander Akers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
